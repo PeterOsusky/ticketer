ef46911f86a08510197dbd26e790964e167a1146
Peter Osusky <peterosusky94@gmail.com>
2023-08-08 17:02:55 +0200

    Initial Commit

---
+++ Ticketer/TicketerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketerApp.swift
+//  Ticketer
+//
+//  Created by Peter on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TicketerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
